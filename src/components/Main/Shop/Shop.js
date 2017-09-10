7ef39fda5ff5b303bdad19b70b6e3bc384381ce5
QuangND18 <quandnd2215@gmail.com>
2017-09-10 15:04:47 +0700

    TabNavigator

--- src/components/Main/Shop/Shop.js
+++ src/components/Main/Shop/Shop.js
@@ -1,18 +1,53 @@
 import React, { Component } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
+import TabNavigator from 'react-native-tab-navigator';
+import Home from './Home/Home';
+import Contact from './Contact/Contact';
+import Cart from './Cart/Cart';
+import Search from './Search/Search';
 
 class Shop extends Component {
-    gotoMenu() {
+    constructor(props) {
+        super(props);
+        this.state = { selectedTab: 'Home' };
+    }
+    openMenu() {
         const { open } = this.props;
         open();
     }
     render() {
         return (
             <View style={{ flex: 1, backgroundColor: '#2A8243' }}>
-                <Text>Shop</Text>
-                <TouchableOpacity onPress={this.gotoMenu.bind(this)}>
-                    <Text>Open Menu</Text>
-                </TouchableOpacity>
+                <TabNavigator>
+                    <TabNavigator.Item
+                        selected={this.state.selectedTab === 'Home'}
+                        title="Home"
+                        onPress={() => this.setState({ selectedTab: 'Home' })}
+                    >
+                    <Home />
+                    </TabNavigator.Item>
+                    <TabNavigator.Item
+                        selected={this.state.selectedTab === 'Search'}
+                        title="Search"
+                        onPress={() => this.setState({ selectedTab: 'Search' })}
+                    >
+                    <Search />
+                    </TabNavigator.Item>
+                    <TabNavigator.Item
+                        selected={this.state.selectedTab === 'Cart'}
+                        title="Cart"
+                        onPress={() => this.setState({ selectedTab: 'Cart' })}
+                    >
+                    <Cart />
+                    </TabNavigator.Item>
+                    <TabNavigator.Item
+                        selected={this.state.selectedTab === 'Contact'}
+                        title="Contact"
+                        onPress={() => this.setState({ selectedTab: 'Contact' })}
+                    >
+                    <Contact />
+                    </TabNavigator.Item>
+                </TabNavigator>
             </View>
         );
     }
